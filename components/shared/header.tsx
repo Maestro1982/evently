1ed28fa213fdddcf17113f33edbe812af8294676
Maestro1982 <kris.tote@telenet.be>
2023-12-20 12:53:09 +0100

    modify footer.tsx and header.tsx in components shared folder, create layout.tsx in app (auth) folder,  mobile-nav.tsx and nav-items.tsx in components shared folder, index.ts in constants folder and install separator.tsx, sheet.tsx from shadcn

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { SignedOut } from '@clerk/nextjs';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 
 import { Button } from '@/components/ui/button';
+
+import NavItems from './nav-items';
+import MobileNav from './mobile-nav';
 
 const Header = () => {
   return (
@@ -17,7 +20,17 @@ const Header = () => {
           />
         </Link>
 
+        <SignedIn>
+          <nav className='md:flex-between hidden w-full max-w-xs'>
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className='w-32 flex justify-end gap-3'>
+          <SignedIn>
+            <UserButton afterSignOutUrl='/' />
+            <MobileNav />
+          </SignedIn>
           <SignedOut>
             <Button asChild className='rounded-full' size='lg'>
               <Link href='/sign-in'>Login</Link>
